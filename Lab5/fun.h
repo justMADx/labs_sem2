268894c8ac800b45d96723885fbda1dc253d023f
MAD x <maksim_ad2@mail.ru>
2022-04-23 14:40:12 +0300

    #####

--- Lab5/fun.h
+++ Lab5/fun.h
@@ -1,18 +1,41 @@
 #pragma once
+#include<iostream>
 #include "fun.h"
 #include <vector>
 using namespace std;
 namespace fun {
 template<typename T>
-T get_max(const vector<T>& array);
+T get_max(const vector<T>& array){
+	if (array.size() <= 0)
+		throw
+		out_of_range{ "Out of range" };
+
+	T max = array[0];
+
+	for (int i = 0; i < array.size(); i++) {
+		if (array[i] > max)
+			max = array[i];
+	}
+	return max;
+};
 
 template<size_t N, typename T>
 class Queue {
 private:
 	vector<T>q;
 public:
-	int MAX = N;
-	void push(T value);
-	T pop();
+	void push(const T& value) {
+		if (q.size() == N)
+			throw out_of_range{ "" };
+		q.push_back(value);
+	};
+	T pop() {
+		if (q.empty())
+			throw
+			runtime_error{ "Runtime error" };
+		int el = q.front();
+		q.erase(q.begin());
+		return el;
+	};
 };
 }
--- Lab5/lab_prog_5.cpp
+++ Lab5/lab_prog_5.cpp
@@ -1,28 +1,46 @@
 ﻿#include <iostream>
 #include <vector>
 #include "fun.h"
-#include "fun.cpp"
 using namespace std;
 using namespace fun;
 
 int main()
 {
-	vector<int> a(0);
+	vector<int> a(1);
 	cout << "==============\n";
+	if (a.size() != 0) cout << "Gimme ur value\n";
 	for (int i : a) {
 		cin >> i;
 		a.push_back(i);
 	}
-	int max = get_max(a);
-	cout <<"Max element: "<<max << '\n';
+	try {
+		int max = get_max(a);
+		cout << "Max element: " << max << '\n';
+	}
+	catch (const exception& e) {
+		cerr << "Caught: " << e.what() << endl;
+		cerr << "Type: " << typeid(e).name() << '\n';
+	}
 	cout << "==============\n";
-	Queue<2, int> MyQ;
-	MyQ.push(1);
-	MyQ.push(2);
-	MyQ.pop();
-	MyQ.pop();
-	MyQ.pop();
-	cout << "==============\n";
-	return 0;
+	Queue<0, int> MyQ;
+	char command;
+	try {
+		MyQ.push(1);
+	}
+	catch (out_of_range) {
+		cout << "Queue is full\n";
+	}
+	try {
+		MyQ.pop();
+		MyQ.pop();
+	}
+	catch (const exception& e) {
+		{
+			cerr << "Caught: " << e.what() << '\n';
+			cerr << "Type: " << typeid(e).name() << '\n';
+			cerr << "Queue is empty\n"; }
+		cout << "==============\n";
+		return 0;
+	}
 }
 
